0920d8670878a05945b82ea8a41b37da983f33ea
Moein <moein.karami@ut.ac.ir>
2020-12-09 15:36:40 +0330

    part 4 done

--- Project4/D-latch.sv
+++ Project4/D-latch.sv
@@ -17,3 +17,12 @@ module Clocked_Dlatch_With_Delay_P2(input D, clk, output Q, Q_bar);
 	nand #8 n3(Q, w1, Q_bar);
 	nand #8 n4(Q_bar, w2, Q);
 endmodule
+
+module Clocked_Dlatch_With_Delay_P2(input D, clk, rst, output Q, Q_bar);
+	wire w1, w2;
+
+	nand #8 n1(w1, D, clk, ~rst);
+	nand #8 n2(w2, clk, w1);
+	nand #8 n3(Q, w1, Q_bar);
+	nand #8 n4(Q_bar, w2, Q);
+endmodule
